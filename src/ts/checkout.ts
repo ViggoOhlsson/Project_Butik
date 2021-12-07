d13053dc985f81289fff86d8db4fc5f309580c60
RasWeb <rasmuspalm@fastmail.se>
2021-12-07 11:50:17 +0100

    Added some javascript validation

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,10 +1,29 @@
 window.onload = function () {
   document.getElementById("addItem").addEventListener("click", addValue);
   document.getElementById("removeItem").addEventListener("click", remVal);
+  document.getElementById("submit").addEventListener("click", validate);
 };
 
-let p = "247";
-let price = "$" + `<span>${p}</span>`;
+//let submit = document.getElementById("submit");
+function validate(e) {
+  // Prevents the default validation.
+  e.preventDefault();
+
+  let firstname = document.getElementById("fnamn");
+  let valid = true;
+
+  if (!firstname.value) {
+    let nameError = document.getElementById("nameError");
+    nameError.classList.add("visible");
+    firstname.classList.add("invalid");
+    nameError.setAttribute("aria-hidden", false);
+    nameError.setAttribute("aria-invalid", true);
+  }
+  return valid;
+}
+
+let p: number = 247;
+let price: any = "$" + `<span>${p}</span>`;
 
 function addValue() {
   let val = parseInt(document.getElementById("item-numb").value, 10);
@@ -17,6 +36,7 @@ function addValue() {
 
 function remVal() {
   let val = parseInt(document.getElementById("item-numb").value, 10);
+  // Checks if val is a number;
   val = isNaN(val) ? 0 : val;
   val < 1 ? (val = 1) : "";
   val--;
