6152eb40a80ffcfd644ae3110654ad6ea8d3805b
RasWeb <rasmuspalm@fastmail.se>
2021-12-07 11:01:14 +0100

    added a item quantity count in js

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,6 +1,26 @@
 window.onload = function () {
-  document.getElementById("my").addEventListener("click", time);
+  document.getElementById("addItem").addEventListener("click", addValue);
+  document.getElementById("removeItem").addEventListener("click", remVal);
 };
 
-// For adding items
-// https://stackoverflow.com/questions/47426929/javascript-adjust-price-when-changing-quantity
+let p = "247";
+let price = " Kostnad: $" + `<span>${p}</span>`;
+
+function addValue() {
+  let val = parseInt(document.getElementById("item-numb").value, 10);
+  val = isNaN(val) ? 0 : val;
+  val++;
+  document.getElementById("item-numb").value = val;
+  price = "$" + `<span>${p * val}</span>`;
+  document.getElementById("price-cont").innerHTML = price;
+}
+
+function remVal() {
+  let val = parseInt(document.getElementById("item-numb").value, 10);
+  val = isNaN(val) ? 0 : val;
+  val < 1 ? (val = 1) : "";
+  val--;
+  document.getElementById("item-numb").value = val;
+  price = "$" + `<span>${p * val}</span>`;
+  document.getElementById("price-cont").innerHTML = price;
+}
