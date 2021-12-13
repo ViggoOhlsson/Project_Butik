ec17ce1ad05a4ed7dfe6cdec8b211d7301b7b31e
RasWeb <rasmuspalm@fastmail.se>
2021-12-13 12:52:15 +0100

    implemented cypress files and a template file.

---
+++ cypress/integration/testex.spec.js
@@ -0,0 +1,19 @@
+// This is a template test file.
+
+// // A Description of the test:
+// describe("A test", () => {
+//it("it should do something", () => {
+
+// // Finds the host on where to do the test.
+// // 1. Arrange
+//   cy.visit("http://localhost:1234");
+
+//  // Dose something, finds the item.
+//  // 2. Act
+//   cy.get("button").type("example");
+
+// // Verifies that the function do what its suppose to.
+//  // 3. Assert
+//   cy.get(li).should("have.value", 12);
+// });
+//})
