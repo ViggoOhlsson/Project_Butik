c1642c62886d4774546a184f4425870fe9160ef2
RasWeb <rasmuspalm@fastmail.se>
2021-12-14 10:28:09 +0100

    added more test files

---
+++ cypress/integration/categoryPage.spec.ts
@@ -0,0 +1,5 @@
+describe("The Category Page", () => {
+  it("It loads", () => {
+    cy.visit("/html/category.html");
+  });
+});
---
+++ cypress/integration/checkoutPage.spec.ts
@@ -0,0 +1,5 @@
+describe("The Home Page", () => {
+  it("It loads", () => {
+    cy.visit("/html/checkout.html");
+  });
+});
