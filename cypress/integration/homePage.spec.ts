b70a402593c612f7bf4e29df165e56e34b36579d
RasWeb <rasmuspalm@fastmail.se>
2021-12-14 13:30:52 +0100

    Added homePage navbar click test

--- cypress/integration/homePage.spec.ts
+++ cypress/integration/homePage.spec.ts
@@ -1,5 +1,13 @@
 describe("The Home Page", () => {
-  it("It loads", () => {
+  it("tests the Christmas category", () => {
     cy.visit("/");
+
+    cy.get("#nav > a:first").click({ force: true });
+  });
+
+  it("tests the Christmas Rock", () => {
+    cy.visit("/");
+
+    cy.get("#nav > a").eq(1).click({ force: true });
   });
 });
