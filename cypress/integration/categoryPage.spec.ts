689b471b46652d12c47429577e99f69f0b040809
RasWeb <rasmuspalm@fastmail.se>
2021-12-14 15:30:51 +0100

    categoryPage cypress testing

--- cypress/integration/categoryPage.spec.ts
+++ cypress/integration/categoryPage.spec.ts
@@ -1,5 +1,25 @@
 describe("The Category Page", () => {
-  it("It loadz", () => {
-    cy.visit("/html/category.html");
+  it("should click on the first album in the Christmas category site.", () => {
+    cy.visit("/html/category.html?c=christmas");
+
+    cy.get(":nth-child(1) > .cell-cover > .img-shadow-box").click();
+  });
+
+  it("should click on the first album in the Rock category site", () => {
+    cy.visit("/html/category.html?c=rock");
+
+    cy.get(":nth-child(1) > .cell-cover > .img-shadow-box").click();
+  });
+
+  it("should click on Rock to move to the rock category.", () => {
+    cy.visit("/html/category.html?c=christmas");
+
+    cy.get("#nav > a").eq(1).click();
+  });
+
+  it("should click on Christmas to move to the christmas category", () => {
+    cy.visit("/html/category.html?c=christmas");
+
+    cy.get("#nav > a").eq(0).click();
   });
 });
--- cypress/integration/homePage.spec.ts
+++ cypress/integration/homePage.spec.ts
@@ -1,7 +1,18 @@
 describe("The Home Page", () => {
+  it("should open the top navigation bar.", () => {
+    cy.visit("/");
+    // Finds the top nav-bar and clicks it.
+    cy.get("#hamburgerButton > .fa").click({ force: true });
+  });
+
+  // // testing video/mp4
+  // //source src= assets/record-player-stock-footage.mp4
+  // it("should load the image", () => {
+  //   cy.visit("/");
+  // });
+
   it("should open the Christmas category", () => {
     cy.visit("/");
-
     // Finds the first <a> in the <nav id=nav>.
     // {force: true} forces cypress to open the link.
     cy.get("#nav > a:first").click({ force: true });
@@ -9,35 +20,25 @@ describe("The Home Page", () => {
 
   it("should open the Rock category", () => {
     cy.visit("/");
-
     // eq(1) is the second element, eq(0) is the first.
     cy.get("#nav > a").eq(1).click({ force: true });
   });
 
-  it("should open the Christmas category using the 'See more' button", () => {
-    cy.visit("/");
-
-    // Finds the moreChristmas id and clicks it.
-    cy.get("#moreChristmas").click({ force: true });
-  });
-
-  it("should open the Rock category using the 'See more' button.", () => {
-    cy.visit("/");
-
-    // Finds the moreRock id and clicks it.
-    cy.get("#moreRock").click({ force: true });
-  });
-
   it("should open the details site for the first Christmas item.", () => {
     cy.visit("/");
-
     // Finds the first Christmas album and clicks it.
     cy.get(
       "#catChristmas > :nth-child(1) > .cell-cover > .img-shadow-box"
     ).click({ force: true });
   });
 
-  it("should open the details site for the first rock item.", () => {
+  it("should open the Christmas category using the 'See more' button", () => {
+    cy.visit("/");
+    // Finds the moreChristmas id and clicks it.
+    cy.get("#moreChristmas").click({ force: true });
+  });
+
+  it("should open the details site for the first Rock item.", () => {
     cy.visit("/");
     // Finds the first rock item and clicks it.
     cy.get("#catRock > :nth-child(1) > .cell-cover > .img-shadow-box").click({
@@ -45,10 +46,9 @@ describe("The Home Page", () => {
     });
   });
 
-  it("should open the top navigation bar.", () => {
+  it("should open the Rock category using the 'See more' button.", () => {
     cy.visit("/");
-    // Finds the top nav-bar and clicks it.
-    cy.get("#hamburgerButton > .fa").click({ force: true });
+    // Finds the moreRock id and clicks it.
+    cy.get("#moreRock").click({ force: true });
   });
-  // it(test);
 });
