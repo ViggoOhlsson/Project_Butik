7d9f21a0422e8b24aba1143321f2381b377b0013
RasWeb <rasmuspalm@fastmail.se>
2021-12-15 15:47:25 +0100

    some small styling

--- src/ts/thankyou.ts
+++ src/ts/thankyou.ts
@@ -1,5 +1,5 @@
 window.onload = function () {
-  setTimeout(function () {
-    window.location.href = "http://localhost:1234";
-  }, 3000);
+  // setTimeout(function () {
+  //   window.location.href = "http://localhost:1234";
+  // }, 3000);
 };
